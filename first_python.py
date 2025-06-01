b18e81455ec99a21376986780c216076fc55996f
Sandy Andos <1sandyandos@gmail.com>
2025-06-01 11:40:22 +0300

    first commit

---
+++ first_python.py
@@ -0,0 +1,7 @@
+#Write a Python program that asks the user for the following information:
+ #   Name, Age, GPA (0-5, can be decimals), Field of Interest, Graduated ("yes" or "no").
+#1- Print a nicely formatted 
+#2- Based on the info, determine and print:
+#If the user is eligible for a scholarship (age < 25, GPA ≥ 3.5, has graduated)
+#If the user is eligible for an internship (age < 30, GPA ≥ 2.5)
+#If neither, recommend they apply again later.
